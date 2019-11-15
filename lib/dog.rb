d6dd238bf8c614fa0bdc7f4f4999f2dfd3aaa342
Anika Christopher <anika.mchristopher@gmail.com>
2019-11-15 01:29:07 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,27 @@
 class Dog
   # code goes here
-end
+  
+  attr_reader :name
+  attr_accessor :owner, :mood
+  
+  @@all = []
+  
+  def initialize(name, owner)
+    @name = name
+    @owner = owner
+    @mood = "nervous"
+    @@all << self
+  end
+  
+  def new_owner
+    new_owner = Owner.new 
+  end
+  
+   def mood
+    @mood 
+  end
+
+  def self.all
+    @@all
+  end
+end 
--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,57 @@
 class Owner
   # code goes here
+  attr_reader :name, :species
+
+  @@all = []
+  
+ def initialize(name, species = "human") 
+   @name = name
+   @species = species
+   @@all << self
+ end 
+ 
+ def say_species
+   return "I am a #{species}."
+ end
+ 
+ def self.all
+   @@all
+ end
+ 
+ def self.count
+   @@all.count
+ end
+ 
+ def self.reset_all
+   self.all.clear
+ end
+ 
+ def cats
+    @cats = Cat.all.select { |cat_obj| cat_obj.owner == self }
+    @cats 
+ end
+ 
+ def dogs
+    @dogs = Dog.all.select { |dog_obj| dog_obj.owner == self }
+    @dogs
+ end
+  
+ def buy_cat(name)
+   @cats = Cat.new(name, self)
+ end
+ 
+ def buy_dog(name)
+   @dogs = Dog.new(name, self)
+ end
+ 
+# def walk_dogs
+#   @@Dogs.each do {|dog| puts "happy"}
+#   end
+# end
+ 
+  # def walk_dogs
+  #   @@pets[:dogs].each do |dog|
+  #     dog.mood = 'happy'
+  #   end
+ 
 end
